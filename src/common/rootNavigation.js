edb336e493a5564116976ec0f1a76f3d0a610d19
mondara1234 <kakzadsr@gmail.com>
2019-04-27 01:30:48 +0700

    Update rootNavigation.js

--- src/common/rootNavigation.js
+++ src/common/rootNavigation.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { DrawerNavigator, StackNavigator } from 'react-navigation';
 import { HEADER_STYLE } from './constants';
 import { HomeRouter } from "../features/Index/router";
-import { SettingRouter, SETTING_SCREEN } from "../features/Setting/router";
+import { SettingRouter } from "../features/Setting/router";
 import { HistoryRouter } from "../features/Treatment_History/router";
-import { UserRouter, LOGIN, REGISTRATION } from "../features/login/router";
+import { UserRouter, LOGIN } from "../features/login/router";
 
 export const RootStack = StackNavigator({
     ...HomeRouter,
@@ -12,7 +12,7 @@ export const RootStack = StackNavigator({
     ...HistoryRouter,
     ...UserRouter
 },{
-    initialRouteName: REGISTRATION,
+    initialRouteName: LOGIN,
     navigationOptions: ({navigation}) => ({
         ...HEADER_STYLE
     }),
